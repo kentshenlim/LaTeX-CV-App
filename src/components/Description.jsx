592e090835da190b0b0cf5a812c2f3da2905f92a
kentshenlim <kentshen_lim24@hotmail.com>
2023-07-30 18:23:02 +0100

    Split css

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
+import './Description.css';
 
 export default function Description({ descriptionArr, updateDescriptionArr }) {
   function handleChange(e, id) {
