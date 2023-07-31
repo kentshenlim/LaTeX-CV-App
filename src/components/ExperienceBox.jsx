22841fc1cd1322ebf7a8dc119938ddf2a395da94
kentshenlim <kentshen_lim24@hotmail.com>
2023-07-31 11:20:43 +0100

    Hook title

--- src/components/ExperienceBox.jsx
+++ src/components/ExperienceBox.jsx
@@ -1,11 +1,32 @@
+import FormBox from './FormBox';
 import PropTypes from 'prop-types';
 
 export default function ExperienceBox({
   experienceDetails,
   setExperienceDetails,
 }) {
-  console.log(experienceDetails, setExperienceDetails);
-  return <>Hi</>;
+  function handleChangeExperienceDetails(e, id) {
+    const newArr = experienceDetails.map((obj) => {
+      if (obj.id !== id) return obj;
+      return { ...obj, [e.target.name]: e.target.value };
+    });
+    setExperienceDetails(newArr);
+  }
+  const arrJSX = experienceDetails.map((obj) => (
+    <div className="experience-box-wrapper" key={obj.id}>
+      <FormBox
+        ionIconName="locate-outline"
+        inputType="text"
+        inputID="title"
+        labelText="Title"
+        placeholderText="The Odin Project"
+        isRequired={true}
+        value={obj.title}
+        handleChange={(e) => handleChangeExperienceDetails(e, obj.id)}
+      ></FormBox>
+    </div>
+  ));
+  return <>{arrJSX}</>;
 }
 
 ExperienceBox.propTypes = {
