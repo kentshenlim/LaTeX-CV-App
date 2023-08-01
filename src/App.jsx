8a49d8888d4a922d9aba4b532877dfd2b14b9c13
kentshenlim <kentshen_lim24@hotmail.com>
2023-08-01 15:03:34 +0100

    Load from localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -2,50 +2,77 @@ import Split from 'react-split';
 import Form from './components/Form';
 import Viewer from './components/Viewer';
 import { useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [personalDetails, setPersonalDetails] = useState({
-    name: '',
-    email: '',
-    gitHub: '',
-    linkedIn: '',
+  const [personalDetails, setPersonalDetails] = useState(() => {
+    const prevData = JSON.parse(localStorage.getItem('latex-cv-forge'));
+    if (prevData) return prevData.personalDetails;
+    return {
+      name: '',
+      email: '',
+      gitHub: '',
+      linkedIn: '',
+    };
   });
 
-  const [educationDetails, setEducationDetails] = useState([
-    // List of objects
-    {
-      id: 0,
-      institute: '',
-      period: '',
-      course: '',
-      result: '',
-      description: [['', 0]], // Array of tuples: [str, id]
-    },
-  ]);
+  const [educationDetails, setEducationDetails] = useState(() => {
+    const prevData = JSON.parse(localStorage.getItem('latex-cv-forge'));
+    if (prevData) return prevData.educationDetails;
+    return [
+      // Array of objects
+      {
+        id: 0,
+        institute: '',
+        period: '',
+        course: '',
+        result: '',
+        description: [['', 0]], // Array of tuples: [str, id]
+      },
+    ];
+  });
 
-  const [experienceDetails, setExperienceDetails] = useState([
-    {
-      id: 0,
-      title: '',
-      period: '',
-      description: [['', 0]], // Array of tuples: [str, id]
-    },
-  ]);
+  const [experienceDetails, setExperienceDetails] = useState(() => {
+    const prevData = JSON.parse(localStorage.getItem('latex-cv-forge'));
+    if (prevData) return prevData.experienceDetails;
+    return [
+      {
+        id: 0,
+        title: '',
+        period: '',
+        description: [['', 0]], // Array of tuples: [str, id]
+      },
+    ];
+  });
 
-  const [skillsDetails, setSkillsDetails] = useState([
-    {
-      id: 0,
-      category: '',
-      descriptionStr: '',
-    },
-  ]);
+  const [skillsDetails, setSkillsDetails] = useState(() => {
+    const prevData = JSON.parse(localStorage.getItem('latex-cv-forge'));
+    if (prevData) return prevData.skillsDetails;
+    return [
+      {
+        id: 0,
+        category: '',
+        descriptionStr: '',
+      },
+    ];
+  });
 
   const [viewerAddress, setViewerAddress] = useState(
     'https://www.africau.edu/images/default/sample.pdf'
   );
 
   const [isBusy, setIsBusy] = useState(false);
+
+  useEffect(() => {
+    const jsonObj = {
+      personalDetails: personalDetails,
+      educationDetails: educationDetails,
+      experienceDetails: experienceDetails,
+      skillsDetails: skillsDetails,
+    };
+    localStorage.setItem('latex-cv-forge', JSON.stringify(jsonObj));
+  }, [personalDetails, educationDetails, experienceDetails, skillsDetails]);
 
   return (
     <main className="app-wrapper">
